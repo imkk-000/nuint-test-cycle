55bfe0b315910b37dfc697b025c4025fa7a46a94
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-07-29 15:18:30 +0700

    Add: Attribute SetUp and TearDown

--- Tests.cs
+++ Tests.cs
@@ -6,10 +6,30 @@ namespace Tests
     [TestFixture]
     public class Tests
     {
-        [Test]
-        public void HelloMe()
+        private int[] array;
+
+        [SetUp]
+        public void TestSetUp()
         {
-            int[] array = new int[] { 1, 2, 3, 4 };
+            array = new int[] { 1, 2, 3, 4 };
+        }
+
+        [TearDown]
+        public void TestTearDown()
+        {
+            array = null;
+        }
+
+        [Test]
+        public void TestWithEqual()
+        {
+            Assert.AreEqual(4, array.Length);
+            Assert.That(array.Length, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void TestWithExactly()
+        {
             Assert.That(array, Has.Exactly(1).EqualTo(3));
             Assert.That(array, Has.Exactly(3).GreaterThan(1));
             Assert.That(array, Has.Exactly(4).LessThan(100));
