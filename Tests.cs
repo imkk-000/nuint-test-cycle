1de4cbae3635a704642b412c73d0b43d1b2c0fd1
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-07-29 14:51:00 +0700

    Add: Assert.That with Has.Exactly

--- Tests.cs
+++ Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace Tests
@@ -8,9 +9,10 @@ namespace Tests
         [Test]
         public void HelloMe()
         {
-            int[] array = new int[]{1, 2, 3, 4, 5};
-            // it can custom assert message and result :)
-            Assert.That(false, "HelloMe Array", array);
+            int[] array = new int[] { 1, 2, 3, 4 };
+            Assert.That(array, Has.Exactly(1).EqualTo(3));
+            Assert.That(array, Has.Exactly(3).GreaterThan(1));
+            Assert.That(array, Has.Exactly(4).LessThan(100));
         }
     }
 }
